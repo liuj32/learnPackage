211026db61cb95fb6e2ba3d9209c6996331bdb63
刘杰 <593177117@qq.com>
2019-08-30 18:00:02 +0800

    node

--- node/node1/main.js
+++ node/node1/main.js
@@ -1,6 +1,23 @@
-var counter1 = require('./src/count');
-var  counter2 = require('./src/count');
+var fs = require('fs');
 
-console.log(counter1.i);
-console.log(counter1.count())
-console.log(counter1.count());
+function copy(src, dst) {
+		fs.writeFileSync(dst, fs.readFileSync(src));
+		//   fs.createReadStream(src).pipe(fs.createWriteStream(dst));
+}
+
+function main(argv) {
+    copy(argv[0], argv[1]);
+}
+
+//main(process.argv.slice(2));
+
+var arr = ['a', {b: 'bb'}]
+var arr1 = arr.slice()
+arr1[1].b = 'aa'
+console.log(arr); // => <Buffer 68 65 65 6c 6f>
+console.log(arr1); // => <Buffer 68 65 65 6c 6f>
+
+/**\
+ * path路径
+ * http://nqdeng.github.io/7-days-nodejs/#3.2.4
+ */
